32fafdccd4e3f03980064b09e0a1a91d8b02db51
ryantheengineer <ryan.larson.wa@gmail.com>
2020-03-19 12:38:39 -0600

    Beginning to implement pandas dataframes

--- calorie_density.py
+++ calorie_density.py
@@ -1,5 +1,9 @@
 import numpy as np
 import datetime as dt
+import pandas as pd
+
+# REWORK USING PANDAS DATAFRAMES
+
 
 # Dictionary of general categories and their calorie per lb density
 densities = {"Vegetables"   : 100.0,
@@ -20,14 +24,16 @@ class Meal:
         self.category = category
         self.weight = weight
         self.date = dt.date.today()
-        self.list = [[category,weight]]
+        # self.list = [[category,weight]]
+        self.list = pd.DataFrame({
+            "Food":     [self.category],
+            "Weight":   [self.weight]})
 
     def add_item(self,category,weight):
         self.list.append([category,weight])
 
     def change_date(self,year,month,day):
         self.date = dt.date(year,month,day)
-
 
 
 
@@ -40,6 +46,22 @@ def get_mealweight(Meal):
         mealweight += row[1]
     return mealweight
 
+# Get the weight of all meals on a given day
+def get_days_mealweight(Meals,querydate):
+    mealweight = 0.0
+    for Meal in Meals:
+        if Meal.date == querydate:
+            mealweight += get_mealweight(Meal)
+    return mealweight
+
+# Get the weight of all meals in a given range of dates, inclusive
+def get_range_mealweight(Meals,startdate,enddate):
+    mealweight = 0.0
+    for Meal in Meals:
+        if Meal.date >= startdate and Meal.date <= enddate:
+            mealweight += get_mealweight(Meal)
+    return mealweight
+
 
 # Get the average calorie density for a given meal
 def avg_cal_density_meal(Meal):
@@ -48,3 +70,15 @@ def avg_cal_density_meal(Meal):
     for row in Meal.list:
         avg_cal_density += row[1]*densities[row[0]]/weightsum
     return avg_cal_density
+
+# Get the average calorie density for a specified day
+def avg_cal_density_day(Meals,querydate):
+    avg_cal_density = 0.0
+    weightsum = get_days_mealweight(Meals,querydate)
+    for Meal in Meals:
+        if Meal.date == querydate:
+            for row in Meal.list:
+                avg_cal_density += row[1]*densities[row[0]]/weightsum
+    return avg_cal_density
+
+# Get the average calorie density over a given date range
